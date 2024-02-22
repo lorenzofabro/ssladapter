b4a9e8529a1a8811d27e324f80644684c678a9da
Lorenzo Fabro <lorenzofabro@Lorenzos-MacBook-Pro.local>
2024-02-22 09:53:31 +0100

    package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ssladapter',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['requests==2.27.1'],
+    python_requires='>=3.6',
+    author='Lorenzo Fabro',
+    author_email='lorenzofabro1997@gmail.com',
+    description='SSL Adapter for requests library',
+    url='https://github.com/your_username/your_package_name',
+)
